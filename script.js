218b2849b2a682c3a4c0a3e2a5bcb77722c3848e
salmaben22 <s_benlem@live.concordia.ca>
2024-10-12 15:30:05 -0400

    Updated script.js
    added admin link

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const wrapper = document.querySelector('.wrapper');
 const loginLink= document.querySelector('.login-link');
 const registerLink =  document.querySelector('.register-link');
+const adminLink =  document.querySelector('.admin-link');
+const adminPopupBtn = document.querySelector('.btnAdmin-popup');
 
-registerLink.addEventListener('click', ()=>{wrapper.classList.add('active');})
-loginLink.addEventListener('click', ()=>{wrapper.classList.remove('active');})
+
+registerLink.addEventListener('click', ()=>{wrapper.classList.add('active');});
+loginLink.addEventListener('click', ()=>{wrapper.classList.remove('active', 'admin-active');});
+adminLink.addEventListener('click', ()=>{wrapper.classList.add('admin-active');});
+adminPopupBtn.addEventListener('click', () => {wrapper.classList.add('admin-active');});
+
+
